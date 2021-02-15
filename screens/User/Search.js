f8d120585d7299711fbaf38be0f7f45d95a7e98c
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2021-02-15 17:09:47 +0700

    99

--- screens/User/Search.js
+++ screens/User/Search.js
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
-import { Button } from "react-native";
-import { View, StyleSheet,Image, Text, SafeAreaView,TouchableOpacity, TextInput } from "react-native";
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default Search = ({navigation})  => {
-    return (
-      
-      <SafeAreaView style={styles.container}>  
-        
-        <Text style={styles.TitleName}>Search</Text>
-           
-      </SafeAreaView>
-      
-    );
-  }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F6E4E4',
-    },
+export default function Search() {
+  return (
     
-   
+    <View style={styles.container}>
+      <MapView style={styles.map}
+      initialRegion={{
+        latitude: 19.027943,
+        longitude: 99.900324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      />
+      </View>
+    
+  );
+}
 
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+ 
+});
