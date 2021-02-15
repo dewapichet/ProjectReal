b6b3f1f5e55c1f4a741abd3384c294eacd7f6ba9
tamza1094 <50855674+tamza1094@users.noreply.github.com>
2021-02-15 21:30:21 +0700

    55

--- screens/User/Search.js
+++ screens/User/Search.js
@@ -1,19 +1,29 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
+import Marker from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 
 export default function Search() {
+  
   return (
     
     <View style={styles.container}>
       <MapView style={styles.map}
       initialRegion={{
         latitude: 19.027943,
-        longitude: 99.900324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        longitude: 99.900325,
+        latitudeDelta: 0.0100,
+        longitudeDelta: 0.0091,
       }}
-      />
+      >
+        <MapView.Marker
+            coordinate={{latitude: 19.027500,
+            longitude: 99.8998000}}
+            title={"title"}
+            description={"description"}
+    />
+    </MapView>
       </View>
     
   );
